d25a86ffe30bdf18258483ab259cd109ec6ee00a
mtaylor <mtaylor@0192ed92-7a03-0410-a25b-9323aeb14dbd>
2008-01-28 20:32:04 +0000

    Restore some of the BSS synchronization code, but only add the goto that skips the beacon timer updates for IBSS.
    git-svn-id: http://madwifi-project.org/svn/madwifi/trunk@3290 0192ed92-7a03-0410-a25b-9323aeb14dbd

--- ath/if_ath.c
+++ ath/if_ath.c
@@ -5143,15 +5143,14 @@ ath_beacon_config(struct ath_softc *sc, struct ieee80211vap *vap)
 			 * next beacon timestamp again */
  			nexttbtt = roundup(hw_tsftu + FUDGE, intval);
 		} 
-		else {
+		else if (ic->ic_opmode == IEEE80211_M_IBSS) {
 			if (tsf > hw_tsf) {
 				/* We received a beacon, but the HW TSF has
 				 * not been updated (otherwise hw_tsf > tsf)
 				 * We cannot use the hardware TSF, so we
 				 * wait to synchronize beacons again. */
 				sc->sc_syncbeacon = 1;
-				if (ic->ic_opmode == IEEE80211_M_IBSS)
-					goto ath_beacon_config_debug;
+				goto ath_beacon_config_debug;
 			} else {
 				/* Normal case: we received a beacon to which
 				 * we have synchronized. Make sure that nexttbtt
@@ -5292,9 +5291,7 @@ ath_beacon_config(struct ath_softc *sc, struct ieee80211vap *vap)
 		ath_hal_intrset(ah, sc->sc_imask);
 	}
 
-#if 0
 ath_beacon_config_debug:
-#endif
 	/* We print all debug messages here, in order to preserve the
 	 * time critical aspect of this function */
 	DPRINTF(sc, ATH_DEBUG_BEACON,
